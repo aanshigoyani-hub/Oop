b488de6de98316a4cff98f7092af997c386765e9
aanshigoyani-hub <aanshigoyani@gmail.com>
2026-02-24 11:56:46 +0530

    Update metertofeet.java

--- metertofeet.java
+++ metertofeet.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class p1 {
+public class metertofeet {
     public static void main(String[] args) {  
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter distance in meters: ");
